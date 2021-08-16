de235aa50fac84c046b9ce29e7baf6da31b97602
Paulo Portugal <portugneto@gmail.com>
2021-08-16 19:13:48 -0300

    Criando MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Github_Actions_Training.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
